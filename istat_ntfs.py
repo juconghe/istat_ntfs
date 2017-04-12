881a2d6fbb7a3ebb08356fc4af93156a8faad04a
Jucong He <hejucong1995@gmail.com>
2017-04-11 23:01:46 -0400

    finished parse standard info

--- istat_ntfs.py
+++ istat_ntfs.py
@@ -16,6 +16,12 @@ def as_signed_le(bs):
     result = struct.unpack('<' + signed_format[len(bs)], bs)[0]
     return result
 
+def get_attribute_type(identifier):
+    dic = {16:'$STANDARD_INFORMATION',48:'$FILE_NAME',128:'$DATA', 32:'$ATTRIBUTE_LIST', \
+    64:'$OBJECT_ID',192:'$REPARSE_POINT',144:'$INDEX_ROOT',160:'$INDEX_ALLOCATION', \
+    176:'$BITMAP'}
+    return dic[identifier]
+
 def get_sector_size(f):
     boot_sector = get_boot_sector(f)
     return as_signed_le(boot_sector[11:13])
@@ -53,24 +59,65 @@ def get_boot_sector(f,sector_size=512):
     return f[:sector_size]
 
 def get_entry(f,address):
-    entry_bytes = f[get_mft_address(f):get_mft_address(f)*get_mft_entry_size(f)]
+    entry_bytes = f[get_mft_address(f)+address*get_mft_entry_size(f): \
+    get_mft_address(f)+address*get_mft_entry_size(f)+get_mft_entry_size(f)]
     return entry_bytes
 
 def get_attribute(entry_bytes,offset):
     return entry_bytes[offset:]
 
+def get_attr_flag(flag):
+    dic = {0x0001:'Read Only',0x0002:'Hidden',0x0004:'System',0x0020:'Archive',\
+    0x0040:'Device',0x0080:'#Normal',0x0100:'Temporary',0x0200:'Sparse file',\
+    0x0400:'Reparse file',0x0800:'Compressed',0x1000:'Offline',\
+    0x2000:'Content is not being indexed for faster searches',0x4000:'Encrypted'}
+    return dic[as_signed_le(flag)]
+
+def parse_standard_info(content):
+    create_time = into_localtime_string(as_signed_le(content[:8]))
+    modified_time = into_localtime_string(as_signed_le(content[8:16]))
+    mft_modified_time = into_localtime_string(as_signed_le(content[16:24]))
+    accessed_time = into_localtime_string(as_signed_le(content[24:32]))
+    flags = get_attr_flag(content[32:36])
+    print('Flags: {}'.format(flags))
+    print('Created:    {}'.format(create_time))
+    print('File Modified:  {}'.format(modified_time))
+    print('MFT Modified:   {}'.format(mft_modified_time))
+    print('Accessed:   {}'.format(accessed_time))
+
 def istat_ntfs(f, address, sector_size=512, offset=0):
     data = f.read()
-    print('Bytes Per Sector {}'.format(get_sector_size(data)))
-    print('Sector per Cluster {}'.format(get_sector_per_cluster(data)))
-    print('Cluster Size {}'.format(get_cluster_size(data)))
-    print('Entry Size {}'.format(get_mft_entry_size(data)))
-    print('Index record Size {}'.format(get_index_record_size(data)))
-    # print('Entry Byte {}'.format(get_entry(data,0)[:16]))
+    # print('Bytes Per Sector {}'.format(get_sector_size(data)))
+    # print('Sector per Cluster {}'.format(get_sector_per_cluster(data)))
+    # print('Cluster Size {}'.format(get_cluster_size(data)))
+    # print('Entry Size {}'.format(get_mft_entry_size(data)))
+    # print('Index record Size {}'.format(get_index_record_size(data)))
+    # print('Entry Byte {}'.format(get_entry(data,address)[:16]))
     entry_bytes = get_entry(data,address)
-    attr_offset = as_signed_le(entry_bytes[4:6])
-    first_attr = get_attribute(entry_bytes,attr_offset)
-    print('First attribute is {}'.format(first_attr[:16]))
+    log_file_sequence_num = as_signed_le(entry_bytes[8:16])
+    sequence_value = as_signed_le(entry_bytes[16:18])
+    link_count = as_signed_le(entry_bytes[18:20])
+    print('MFT Entry Header Values:')
+    print('Entry: {}        Sequence: {}'.format(address,sequence_value))
+    print('$LogFile Sequence Number: {}'.format(log_file_sequence_num))
+    print('Allocated File')
+    print('Links: {}'.format(link_count))
+    attr_offset = as_signed_le(entry_bytes[20:22])
+    # print('Offset is {}'.format(attr_offset))
+    attr = get_attribute(entry_bytes,attr_offset)
+    attr_identifier = get_attribute_type(as_signed_le(attr[:4]))
+    attr_size = as_signed_le(attr[4:8])
+    print('{} Attribute Values:'.format(attr_identifier))
+    name_len = as_signed_le(attr[9:10])
+    non_resident_flag = 'Resident' if as_signed_le(attr[8:9])==0 else 'Non-resident'
+    content_size = as_signed_le(attr[16:20]) if non_resident_flag=='Resident' else as_signed_le(attr[48:56])
+    content_offset = as_signed_le(attr[20:22])
+    # print('Name {}'.format(name_len))
+    # print(non_resident_flag)
+    # print('Size {}'.format(content_size))
+    # print('Offset to Content {}'.format(content_offset))
+    content = attr[content_offset:content_offset+content_size]
+    parse_standard_info(content)
 
 
 def into_localtime_string(windows_timestamp):
